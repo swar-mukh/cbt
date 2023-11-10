b909a2b194f99d6a978ee8b1f2298708a082fdd6
Swarnava Mukherjee <swarnava@getdefault.in>
2023-11-10 13:09:49 +0530

    tool: Use C++17 standard throughout

--- script.sh
+++ script.sh
@@ -4,12 +4,12 @@ echo "Deleting sample project (if present)" && rm -rf my-project
 
 echo
 
-echo "Compiling src/util/scaffold.cpp" && g++ -Wall -Wextra -pedantic -c -I ./headers/ src/util/scaffold.cpp -o ./build/binaries/util/text_content.o
-echo "Compiling src/commands.cpp" && g++ -Wall -Wextra -pedantic -c -I ./headers/ src/commands.cpp -o ./build/binaries/commands.o
-echo "Compiling src/main.cpp" && g++ -Wall -Wextra -pedantic -c -I ./headers/ src/main.cpp -o ./build/binaries/main.o
+echo "Compiling src/util/scaffold.cpp" && g++ -std=c++17 -Wall -Wextra -pedantic -c -I ./headers/ src/util/scaffold.cpp -o ./build/binaries/util/text_content.o
+echo "Compiling src/commands.cpp" && g++ -std=c++17 -Wall -Wextra -pedantic -c -I ./headers/ src/commands.cpp -o ./build/binaries/commands.o
+echo "Compiling src/main.cpp" && g++ -std=c++17 -Wall -Wextra -pedantic -c -I ./headers/ src/main.cpp -o ./build/binaries/main.o
 
 echo
 
-echo "Building build/cbt" && g++ -O3 build/binaries/*.o build/binaries/**/*.o -lstdc++fs -o build/cbt
+echo "Building build/cbt" && g++ -std=c++17 -Wall -Wextra -pedantic -O3 build/binaries/*.o build/binaries/**/*.o -o build/cbt
 
 echo
