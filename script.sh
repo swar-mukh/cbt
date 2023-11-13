b802d0fe0a4bd53c50fc7c747095d808da012eff
Swarnava Mukherjee <swarnava@getdefault.in>
2023-11-13 21:36:29 +0530

    tool: Update helper script and upgrade to C++2a standard

--- script.sh
+++ script.sh
@@ -4,12 +4,13 @@ echo "Deleting sample project (if present)" && rm -rf my-project
 
 echo
 
-echo "Compiling src/workspace/scaffold.cpp" && g++ -std=c++17 -Wall -Wextra -pedantic -c -I ./headers/ src/workspace/scaffold.cpp -o ./build/binaries/workspace/scaffold.o
-echo "Compiling src/commands.cpp" && g++ -std=c++17 -Wall -Wextra -pedantic -c -I ./headers/ src/commands.cpp -o ./build/binaries/commands.o
-echo "Compiling src/main.cpp" && g++ -std=c++17 -Wall -Wextra -pedantic -c -I ./headers/ src/main.cpp -o ./build/binaries/main.o
+echo "Compiling src/workspace/scaffold.cpp" && g++ -std=c++2a -Wall -Wextra -pedantic -c -I ./headers/ src/workspace/scaffold.cpp -o ./build/binaries/workspace/scaffold.o
+echo "Compiling src/workspace/util.cpp" && g++ -std=c++2a -Wall -Wextra -pedantic -c -I ./headers/ src/workspace/util.cpp -o ./build/binaries/workspace/util.o
+echo "Compiling src/commands.cpp" && g++ -std=c++2a -Wall -Wextra -pedantic -c -I ./headers/ src/commands.cpp -o ./build/binaries/commands.o
+echo "Compiling src/main.cpp" && g++ -std=c++2a -Wall -Wextra -pedantic -c -I ./headers/ src/main.cpp -o ./build/binaries/main.o
 
 echo
 
-echo "Building build/cbt" && g++ -std=c++17 -Wall -Wextra -pedantic -O3 -Os -s build/binaries/*.o build/binaries/**/*.o -o build/cbt
+echo "Building build/cbt" && g++ -std=c++2a -Wall -Wextra -pedantic -O3 -Os -s build/binaries/*.o build/binaries/**/*.o -o build/cbt
 
 echo
