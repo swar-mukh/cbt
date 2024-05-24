2541bb6c405f2ea574fa15df12060f73dc58cbf3
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2024-05-24 16:48:29 +0530

    feat: Add support for configuration deserialisation
    Signed-off-by: Swarnava Mukherjee <swar_mukh10@yahoo.co.in>

--- src/workspace/project_config.cpp
+++ src/workspace/project_config.cpp
@@ -1,14 +1,19 @@
 #include "workspace/project_config.hpp"
 
 #include <filesystem>
+#include <fstream>
 #include <string>
 
-#include "workspace/scaffold.hpp"
+#include "workspace/util.hpp"
 
 namespace workspace::project_config {
     namespace fs = std::filesystem;
 
     using std::string;
+
+    const string AUTHOR_DELIMITER{ ":" };
+    const string DELIMITER{ "=" };
+    const string LINE_COMMENT{ ";" };
 
     string platform_to_string(const Platform platform) {
         using enum workspace::project_config::Platform;
@@ -35,8 +40,52 @@ namespace workspace::project_config {
     }
 
     Project convert_cfg_to_model() {
-        Project project;
-        return project;
+        const string config_file_name{ "project.cfg" };
+
+        if (fs::exists(config_file_name)) {
+            std::ifstream config_file(config_file_name);
+            string line;
+            int line_number{ 0 };
+
+            Project project;
+
+            while (std::getline(config_file, line)) {
+                ++line_number;
+                std::erase(line, '\r');
+
+                if (line.empty() || line.starts_with(LINE_COMMENT)) {
+                    continue;
+                }
+
+                const auto [key, value] = workspace::util::get_key_value_pair_from_line(line, DELIMITER);
+            
+                if (key.compare("name") == 0) {
+                    project.name = value;
+                } else if (key.compare("description") == 0) {
+                    project.description = value;
+                } else if (key.compare("version") == 0) {
+                    project.version = value;
+                } else if (key.compare("authors[]") == 0) {
+                    const auto [name, email_id] = workspace::util::get_key_value_pair_from_line(value, AUTHOR_DELIMITER);
+                    project.authors.insert(Author{ .name{ name }, .email_id{ email_id } });
+                } else if (key.compare("platforms[]") == 0) {
+                    project.platforms.insert(string_to_platform(value));
+                } else {
+                    throw std::runtime_error("Invalid configuration at line " + std::to_string(line_number) + " for key '" + key + "'");
+                }
+            }
+
+            if (project.authors.size() == 0) {
+                throw std::runtime_error("At least one author is required in 'project.cfg'");
+            }
+            if (project.platforms.size() == 0) {
+                throw std::runtime_error("At least one platform is required in 'project.cfg'");
+            }
+
+            return project;
+        } else {
+            throw std::runtime_error("'project.cfg' missing!");
+        }
     }
 
     string convert_model_to_cfg(const Project project, const bool add_disclaimer_text) {
@@ -51,7 +100,7 @@ namespace workspace::project_config {
         string authors_text{ std::string("; `authors` is always an array even if there is only one entity. At least one author is required.") };
 
         for (const Author &author: project.authors) {
-            authors_text += std::string("\nauthors[]=") + author.name + " <" + author.email_id + ">";
+            authors_text += std::string("\nauthors[]=") + author.name + AUTHOR_DELIMITER + author.email_id;
         }
 
         string platforms_text{ std::string("; `platforms` is always an array even if there is only one supported platform and")
