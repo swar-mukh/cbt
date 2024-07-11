7fd9738875e9912ffd11ceeb3d5d095ecb290d0d
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2024-07-11 23:49:24 +0530

    chore: Bump Release ID
    Signed-off-by: Swarnava Mukherjee <swar_mukh10@yahoo.co.in>

--- src/commands.cpp
+++ src/commands.cpp
@@ -295,7 +295,7 @@ namespace commands {
             << "cbt: C++ Build Tool" << endl
             << endl
             << "Author        - Swarnava Mukherjee" << endl
-            << "Release       - 2024.06.23" << endl
+            << "Release       - 2024.07.12" << endl
             << endl
             << "C++ Standard  - " << __cplusplus << endl
             << "GCC Version   - " << GCC_VERSION << endl
