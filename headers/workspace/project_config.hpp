2693c251b5b2b0c709043003bf4babf652fa1701
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2026-03-25 21:21:56 +0530

    feat: Enhance dependency comparison
    Signed-off-by: Swarnava Mukherjee <swar_mukh10@yahoo.co.in>

--- headers/workspace/project_config.hpp
+++ headers/workspace/project_config.hpp
@@ -39,11 +39,15 @@ namespace workspace::project_config {
         using is_transparent = void;
 
         bool operator()(const SurfaceDependency& lhs, const SurfaceDependency& rhs) const {
-            if (lhs.name == rhs.name) {
-                return lhs.version > rhs.version;
+            if (lhs.name != rhs.name) {
+                return lhs.name < rhs.name;
             }
 
-            return lhs.name < rhs.name;
+            if ((!lhs.url.empty() && !rhs.url.empty()) && lhs.url != rhs.url) {
+                return lhs.url < rhs.url;
+            }
+
+            return lhs.version > rhs.version;
         }
 
         bool operator()(const SurfaceDependency& lhs, const std::string& rhs) const {
